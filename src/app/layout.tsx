de607da511047f7c269ee2324061cbaec20f7f2b
schrodd <andres_alejandro_rodriguez@hotmail.com>
2023-07-03 23:45:08 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-"use client"
-import Link from 'next/link'
 import './globals.css'
 import { Inter } from 'next/font/google'
-import HomeIcon from '@mui/icons-material/Home';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,15 +17,6 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={`${inter.className} bg-gradient-to-tr from-slate-800 to-slate-600 bg-fixed text-white`}>
-        <header className="items-center justify-between m-5 py-1 px-5 cont fixed top-0 left-0 gap-5 z-50 hidden md:flex">
-          <Link href='/' className='text-3xl font-bold'><HomeIcon className='-mt-1'/></Link>
-          <ul className='flex gap-5 font-light text-sm tracking-wide [&>*]:transition'>
-            <li><Link href='#me'>Sobre mí</Link></li>
-            <li><Link href='#stack'>Mi Stack</Link></li>
-            <li><Link href='#projects'>Proyectos</Link></li>
-            <li><Link href='#contact'>Contacto</Link></li>
-          </ul>
-        </header>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,32 @@ import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
 import Link from "next/link";
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import HomeIcon from '@mui/icons-material/Home';
 
 export default function Home() {
+  function scrollTop() {
+    if (window)
+    window.scrollTo(0,0)
+  }
   return (
-    <main className="flex flex-col w-full justify-center items-center">
-      <Top/>
-      <Me/>
-      <Stack/>
-      <Projects/>
-      <Contact/>
-      <Link href='/' className="cont mb-10 px-3 py-1 text-xs"><KeyboardArrowUpIcon className="mr-1 -ml-1"/>Volver arriba</Link>
-    </main>
+    <>
+      <header className="items-center justify-between m-5 py-1 px-5 cont fixed top-0 left-0 gap-5 z-50 hidden md:flex">
+        <Link href='/' onClick={scrollTop} className='text-3xl font-bold'><HomeIcon className='-mt-1'/></Link>
+        <ul className='flex gap-5 font-light text-sm tracking-wide [&>*]:transition'>
+          <li><Link href='#me'>Sobre mí</Link></li>
+          <li><Link href='#stack'>Mi Stack</Link></li>
+          <li><Link href='#projects'>Proyectos</Link></li>
+          <li><Link href='#contact'>Contacto</Link></li>
+        </ul>
+      </header>
+      <main className="flex flex-col w-full justify-center items-center">
+        <Top/>
+        <Me/>
+        <Stack/>
+        <Projects/>
+        <Contact/>
+        <Link href='/' onClick={scrollTop} className="cont mb-10 px-3 py-1 text-xs"><KeyboardArrowUpIcon className="mr-1 -ml-1"/>Volver arriba</Link>
+      </main>
+    </>
   )
 }
