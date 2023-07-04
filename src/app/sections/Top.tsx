5e7368ca40dd4c8854c4f6e22a5acba3773e37cc
schrodd <andres_alejandro_rodriguez@hotmail.com>
2023-07-03 23:54:08 -0300

    fix

--- src/app/sections/Top.tsx
+++ src/app/sections/Top.tsx
@@ -14,11 +14,11 @@ export default function Top(){
         </div>
       </div>
       <div className="relative flex-1 h-full w-screen md:w-auto flex justify-center items-center -order-1 md:order-1
-      mt-5">
+      mt-16 md:mt-5">
         <div className="round w-[70vw] h-[70vw] md:w-[32vw] md:h-[32vw] absolute"></div>
         <Image src='/ilus.svg' alt='illustration' fill className="drop-shadow-xl"/>
       </div>
-      <KeyboardArrowDownIcon className="absolute bottom-5 -mr-3 right-1/2 animate-bounce w-8 h-8"/>
+      <KeyboardArrowDownIcon className="absolute bottom-20 md:bottom-10 -mr-3 right-1/2 animate-bounce w-8 h-8"/>
     </section>
   )
 }
