6c01481ebc65013fb7fdb455d96cbf118a00867c
schrodd <andres_alejandro_rodriguez@hotmail.com>
2023-07-04 00:15:35 -0300

    fix

--- src/app/sections/Top.tsx
+++ src/app/sections/Top.tsx
@@ -5,7 +5,7 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 export default function Top(){
   const tags = ['React','Typescript','Javascript/ES6','NextJS','MaterialUI','Framer Motion','Tailwind','Sass','+']
   return (
-    <section className="flex flex-col md:flex-row md:w-2/3 h-[80vh] md:h-screen items-center relative">
+    <section className="flex flex-col md:flex-row md:w-2/3 h-auto md:h-screen items-center relative">
       <div className="flex-1 md:w-1/2 p-5 md:p-0 relative z-10 text-center md:text-left">
         <h1>Andrés Molina</h1>
         <h2 className="text-lg md:text-3xl mb-5">Desarrollador Frontend</h2>
@@ -13,8 +13,8 @@ export default function Top(){
           {tags.map((e,i) => <p key={i} className="tag">{e}</p>)}
         </div>
       </div>
-      <div className="relative flex-1 h-full w-screen md:w-auto flex justify-center items-center -order-1 md:order-1
-      mt-16 md:mt-5">
+      <div className="relative md:flex-1 h-72 md:h-full w-screen md:w-auto flex justify-center items-center -order-1 md:order-1
+      mt-12 md:mt-5">
         <div className="round w-[70vw] h-[70vw] md:w-[32vw] md:h-[32vw] absolute"></div>
         <Image src='/ilus.svg' alt='illustration' fill className="drop-shadow-xl"/>
       </div>
